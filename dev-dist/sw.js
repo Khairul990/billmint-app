7e1dfa2721a60048f1e1ef8839e5f42c6e681255
Khairul990 <159559482+Khairul990@users.noreply.github.com>
2026-06-26 17:00:19 +0530

    Update BillQyro

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dsva3g4naes"
+    "revision": "0.lamjui2rnjg"
   }], {
     "ignoreURLParametersMatching": [/^token/, /^secret/, /^auth/]
   });
