79f6a6a82211b2224e806fe227908d3e00922ce5
Khairul990 <159559482+Khairul990@users.noreply.github.com>
2026-08-27 22:27:30 +0530

    fix: resolve ESM financial calculation import

--- src/utils/financialCommandCenter.js
+++ src/utils/financialCommandCenter.js
@@ -10,7 +10,7 @@ import {
   getInvoicePaidTotal,
   getInvoiceBalanceDue,
   roundTo2,
-} from './financialCalculations';
+} from './financialCalculations.js';
 
 const isActiveInvoice = (invoice) => (
   invoice &&
